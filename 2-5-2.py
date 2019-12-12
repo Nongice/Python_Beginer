5c2d99dd3418fcb3b459d20f82caf7f8d017bb10
Wasawat Junnasaksri <ice7533678@gmail.com>
2019-12-12 22:05:12 +0700

    Update #1

---
+++ 2-5-2.py
@@ -0,0 +1,7 @@
+st_name = "กรวิชญ์"
+st_surname = " ดวงตา"  # เว้นไว้เพื่อความสวยงาม โดยการรันครั้งนี้ไม่มีผลต่อโค้ด
+mid_sc = 32.50
+final_sc = 35
+all_sc = mid_sc + final_sc
+average = all_sc / 2
+print(st_name + st_surname + "\nมีคะแนนรวมเป็น: ", all_sc, "\nมีคะแนนเฉลี่ยเป็น: ", average)
