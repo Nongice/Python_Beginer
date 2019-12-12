2a24f6a47896a11b15fbd767c64c506a4a35f249
Wasawat Junnasaksri <ice7533678@gmail.com>
2019-12-12 22:42:29 +0700

    เพิ่มคำอธิบาย บรรทัดตัวสุดท้าย

--- ใบงาน 2.5/2-5-1.py
+++ ใบงาน 2.5/2-5-1.py
@@ -1,9 +1,9 @@
-st_name = "กรวิชญ์"
-st_surname = " ดวงตา" # เว้นวรรคไว้เพื่อความสวยงาม โดยการเว้นนี้ไม่มีผลต่อโค้ดที่เขียน
+st_name = "ชื่อ"
+st_surname = " สกุล" # เว้นวรรคไว้เพื่อความสวยงาม โดยการเว้นนี้ไม่มีผลต่อโค้ดที่เขียน
 th_score = 10
 en_score = 12
 social_score = 18
 sci_score = 15
 math_score = 20
 average = (th_score + en_score + social_score + sci_score + math_score) / 5
-print(st_name + st_surname+"ได้คะแนนเฉลี่ยรวม " + '%.2f' % average + " คะแนน")
+print(st_name + st_surname+"ได้คะแนนเฉลี่ยรวม " + '%.2f' % average + " คะแนน")  # '%.2f' % average คือแสดงค่าเฉลี่ยคะแนนเป็นทศนิยม 2 ตำแหน่ง
