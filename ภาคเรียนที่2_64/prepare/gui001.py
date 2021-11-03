667c5ea47746992e5fa09740db3cfdb6443c5b02
Wasawat Junnasaksri <ice7533678@gmail.com>
2021-11-03 23:26:34 +0700

    Fix bug: gui001.py
    แก้ไขบัคสามารถรีเซ็ตการกดปุ่มในขณะที่ไม่เคยกดปุ่มใดๆ

--- ภาคเรียนที่2_64/prepare/gui001.py
+++ ภาคเรียนที่2_64/prepare/gui001.py
@@ -6,21 +6,21 @@ def press():
     global count
     count+=1
     print("Times Press:",count)
-    app.title("Python Beginner - You press me with %s times!"%count)
+    app.title("Python Beginer - You press me with %s times!"%count)
     messagebox.showwarning("Nofications","Hey! You press me {} times.".format(count))
 def reset():
     global count
-    if count > 1:
+    if count > 0:
         count = 0
         print("Reset to:",count)
-        app.title("Python Beginner - You press me with %s times!"%count)
+        app.title("Python Beginer - You press me with %s times!"%count)
         messagebox.showinfo("Nofications","Hey! You are reset.")
     else:
         messagebox.showerror("Nofications","Hey! You can't reset with 0 press time.")
 ######################################################
 app = Tk()
 app.geometry("800x600")
-app.title("Python Beginner - You press me with %s times!"%count)
+app.title("Python Beginer - You press me with %s times!"%count)
 btn_plus_count = Button(text='Click Me!',bg="lightgreen",command=press,height=100,width=100)
 btn_reset_count = Button(text='Reset',bg="red",command=reset).pack(side=BOTTOM, pady=7)
 btn_plus_count.pack(side = TOP, pady = 150)
