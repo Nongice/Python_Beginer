d0f39d7591b443203f7763537b7eb43520909011
Wasawat Junnasaksri <ice7533678@gmail.com>
2021-11-03 23:21:41 +0700

    Update gui001.py
    NEW: Reset press time

--- ภาคเรียนที่2_64/prepare/gui001.py
+++ ภาคเรียนที่2_64/prepare/gui001.py
@@ -8,12 +8,21 @@ def press():
     print("Times Press:",count)
     app.title("Python Beginner - You press me with %s times!"%count)
     messagebox.showwarning("Nofications","Hey! You press me {} times.".format(count))
-
+def reset():
+    global count
+    if count > 1:
+        count = 0
+        print("Reset to:",count)
+        app.title("Python Beginner - You press me with %s times!"%count)
+        messagebox.showinfo("Nofications","Hey! You are reset.")
+    else:
+        messagebox.showerror("Nofications","Hey! You can't reset with 0 press time.")
 ######################################################
 app = Tk()
 app.geometry("800x600")
 app.title("Python Beginner - You press me with %s times!"%count)
-button = Button(text='Click Me!',bg="lightgreen",command=press,height=100,width=100)
-button.pack(side = TOP, pady = 150)
+btn_plus_count = Button(text='Click Me!',bg="lightgreen",command=press,height=100,width=100)
+btn_reset_count = Button(text='Reset',bg="red",command=reset).pack(side=BOTTOM, pady=7)
+btn_plus_count.pack(side = TOP, pady = 150)
 app.mainloop()
 ######################################################
